a6585d014e2f23c64cf53bbcffe05f4763fc6c7d
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2026-06-24 09:53:01 +0800

    fix(website): replace react-shiki with bare shiki and fix the highlighter life warning

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -6,12 +6,6 @@ import { loadFiles } from "./plugin/load_files";
 
 export default defineConfig({
   plugins: [loadFiles(), tailwindcss(), reactRouter()],
-  ssr: {
-    // react-shiki does not support SSR because it imports CSS from JS.
-    // Without this config, the following error will occur.
-    //   [vite] Internal server error: Unknown file extension ".css" for ***/node_modules/.pnpm/react-shiki@***/node_modules/react-shiki/dist/style.css
-    noExternal: ["react-shiki"],
-  },
   resolve: {
     tsconfigPaths: true,
   },
