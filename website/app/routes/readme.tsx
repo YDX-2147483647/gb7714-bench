db8c5a26d836e83848406847da5d500dc1640a77
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2026-07-05 18:48:39 +0800

    fix(website): add the missing page meta for `/`

--- website/app/routes/readme.tsx
+++ website/app/routes/readme.tsx
@@ -2,6 +2,17 @@ import { MarkdownPage } from "~/components/MarkdownPage";
 import { loadReadme } from "~/lib/docs.server";
 import type { Route } from "./+types/readme";
 
+export function meta(_: Route.MetaArgs) {
+  return [
+    { title: "GB/T 7714 格式参考文献引擎哪家强？| GB/T 7714 Benchmark" },
+    {
+      name: "description",
+      content:
+        "利用 Zotero 中文 CSL 开发组测试文献，测试十种支持 GB/T 7714 的参考文献引擎。",
+    },
+  ];
+}
+
 export async function loader(_: Route.LoaderArgs) {
   const readme = await loadReadme();
   return { readme };
